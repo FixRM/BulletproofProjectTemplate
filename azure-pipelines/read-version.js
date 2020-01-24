4f6faf5fd731b294d69549c8813e7af1d693c152
Artem Grunin <FixRM@users.noreply.github.com>
2020-01-24 21:46:29 +0500

    feat: setup azure pipelines

---
+++ azure-pipelines/read-version.js
@@ -0,0 +1,7 @@
+//print package version
+const pack = require("../package.json");
+console.log(`package version ${pack.version}`);
+
+//set output variable
+const devops = require("azure-pipelines-task-lib");
+devops.setVariable("SemVersion", pack.version);
