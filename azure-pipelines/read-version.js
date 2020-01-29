9b9241da9cfd6c6dc13b2f50a45235e550d14757
Artem Grunin <FixRM@users.noreply.github.com>
2020-01-29 12:19:43 +0500

    chore: remove setting version with node

--- azure-pipelines/read-version.js
+++
@@ -1,9 +0,0 @@
-const devops = require("azure-pipelines-task-lib");
-const pack = require("../package.json");
-
-const version = pack.version;
-//print package version
-console.log(`package version ${version}`);
-
-//set variable
-devops.setVariable("SemVer", version);
