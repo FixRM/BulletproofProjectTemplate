996f68f402339823a0fbbdd817c111514d0232ea
Artem Grunin <FixRM@users.noreply.github.com>
2020-01-29 12:16:25 +0500

    fix: fix yaml version bump

--- azure-pipelines/azure-pipeline-updater.js
+++ azure-pipelines/azure-pipeline-updater.js
@@ -1,11 +1,11 @@
 // azure-pipeline-updater.js
 
-const regex = /semver:\s+(\d+\.\d+\.\d+$)/;
+const regex = /semver:\s+(\d+\.\d+\.\d+)/;
 
 module.exports.readVersion = function (contents) {
-    return regex.test(contents)[1];
+    return contents.match(regex)[1];
 };
 
 module.exports.writeVersion = function (contents, version) {
-    return contents.replace(regex, version);
+    return contents.replace(regex, `semver: ${version}`);
 };
