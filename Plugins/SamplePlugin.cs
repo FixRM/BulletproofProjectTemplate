47de374997775f36ab7c584d8b137e89f0a8a9bc
Artem Grunin <FixRM@users.noreply.github.com>
2020-03-24 18:47:30 +0500

    feat: add unit test project

---
+++ Plugins/SamplePlugin.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixRM.Plugins
+{
+    [CrmPluginRegistration(
+        message: "sample",
+        entityLogicalName: null,
+        stage: StageEnum.PostOperation,
+        executionMode: ExecutionModeEnum.Synchronous,
+        filteringAttributes: null,
+        stepName: "FixRM.Plugins.SamplePlugin",
+        executionOrder: 10,
+        isolationModel: IsolationModeEnum.Sandbox)]
+    public class SamplePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
