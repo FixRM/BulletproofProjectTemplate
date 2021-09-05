14ff7e10737e3d515fb2fc464dfb4501c43a5ece
Artem Grunin <FixRM@users.noreply.github.com>
2021-09-05 19:51:02 +0500

    chore: optimize webpack config

---
+++ WebResources/webpack.common.config.ts
@@ -0,0 +1,47 @@
+﻿import { Configuration, HotModuleReplacementPlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ESLintPlugin from "eslint-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import path from "path";
+
+const common: Configuration = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "[name].js",
+        publicPath: "",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            async: false
+        }),
+        new ESLintPlugin({
+            extensions: ["js", "jsx", "ts", "tsx"],
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
+
+export default common;
