d2f9f97a18c216585ae8682f5f284a3e315c31db
Artem Grunin <FixRM@users.noreply.github.com>
2021-09-05 18:36:37 +0500

    feat: webpack setup

---
+++ WebResources/src/index.tsx
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+    <h1>Basic React and TypeScript App</h1>
+);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
